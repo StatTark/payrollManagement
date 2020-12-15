afeacc0482b2298afe5f0a6912de3a27a896d1d8
Ahmet Tarık Şahin <33375656+StatTark@users.noreply.github.com>
2020-12-16 02:35:59 +0300

    Update database.py

--- database.py
+++ database.py
@@ -22,32 +22,3 @@ with open("C:/Users/StatTark/CLionProjects/payroll/cmake-build-debug/databaseCol
                 c.write(str(k)+" ")
             c.write("< ")
             c.write("\n")
-
-
-
-
-
-
-
-'''db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="",
-                     db="payroll")
-cursor = db.cursor()
-"""sorgu="SELECT * from users"
-cursor.execute(sorgu,)
-result = cursor.fetchall()
-print(result)"""
-
-with open("cmake-build-debug/filename.txt","r+") as f:
-    okuma =  f.read()
-    print(okuma +" (Sql sorgusu)")
-    cursor.execute(okuma,)
-    result = cursor.fetchall()
-    f.truncate(0) #dosya içeriği siler
-    with open("cmake-build-debug/filename.txt","a")as c:
-        for i in result:
-            print(i,"\n")
-            c.write(str(i))
-            c.write("\n")
-cursor.close()'''
